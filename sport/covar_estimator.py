e64325571580f94d32dfd90252ff476426668f7e
Yan Zeng <quantsummaries@gmail.com>
2025-04-23 13:40:04 -0400

    re-estimated the covar/corr matrix for the time window [2007-12-11, 2025-02-11]

--- sport/covar_estimator.py
+++ sport/covar_estimator.py
@@ -1,7 +1,10 @@
+# Python standard library: https://docs.python.org/3/library/index.html
+import datetime
 import math
 import os
 from typing import Dict, List
 
+# open source packages
 import pandas as pd
 
 
@@ -19,34 +22,49 @@ class CovarEstimator:
         Returns:
             rtrns (list): list of returns.
         """
+        if price is None:
+            raise ValueError("Input price list is None")
+        if not isinstance(price, list):
+            raise ValueError("Input price list is not of type list")
+        if len(price) == 0:
+            raise ValueError("Input price list is empty")
+        if method is None or not isinstance(method, str) or method not in ('simple', 'logarithm'):
+            raise ValueError("Input method is not valid")
+
         rtrns = list()
-        for idx in range(len(price)):
-            if idx == 0:
-                continue
+        for idx in range(1, len(price)):
+            if method == 'simple':
+                rtrn = price[idx]/price[idx-1] - 1
+            elif method == 'logarithm':
+                rtrn = math.log(price[idx]) - math.log(price[idx-1])
             else:
-                if method == 'simple':
-                    rtrn = price[idx]/price[idx-1] - 1
-                elif method == 'logarithm':
-                    rtrn = math.log(price[idx]) - math.log(price[idx-1])
-                else:
-                    raise ValueError('Input method is invalid: must be log or simple')
+                raise ValueError('Input method is invalid: must be log or simple')
             rtrns.append(rtrn)
 
         return rtrns
 
     @staticmethod
-    def _preprocess_data(data_dir: str) -> tuple:
+    def _preprocess_data(data_dir: str, price_type: str) -> (Dict[str, pd.DataFrame], Dict[str, Dict[str, object]]):
         """Collect price data and other data.
 
         Args:
             data_dir (str): a directory which holds all the security price data.
-
+            price_type (str): type of price used for return calculation, e.g. Close, Adjust Close, etc.
         Returns:
             sec_id_to_price (dict): {sec_id: pandas.DataFrame (columns=['DATE', 'PRICE'])}.
             sec_id_to_last_data (dict): {sec_id: dictionary of data on the last date}.
         """
         if data_dir is None:
             raise ValueError('Input data directory is None')
+        if not isinstance(data_dir, str):
+            raise ValueError(f"""Input data directory is not of type str: {data_dir}""")
+        if not os.path.exists(data_dir):
+            raise ValueError(f"""Input data directory does not exist: {data_dir}""")
+
+        if price_type is None:
+            raise ValueError('Input price type is None')
+        if not isinstance(price_type, str):
+            raise ValueError(f"""Input price type is not of type str: {price_type}""")
 
         sec_id_to_price = dict()
         sec_id_to_last_data = dict()
@@ -62,6 +80,7 @@ class CovarEstimator:
                                           'HIGH': 'float',
                                           'LOW': 'float',
                                           'CLOSE': 'float',
+                                          'VOLUME': 'int',
                                           'NUM_SHARES': 'float',
                                           'AMOUNT': 'float',
                                           'NUM_TRANSACTIONS': 'int'})
@@ -71,8 +90,11 @@ class CovarEstimator:
                     continue
 
                 # choose 'CLOSE' as 'PRICE'
-                price_data = data[['DATE', 'CLOSE']]
+                if price_type not in data.columns:
+                    raise ValueError(f"""price data in {file} does not have a column {price_type} that can be used for return calculation.""")
+                price_data = data[['DATE', price_type]].copy()
                 price_data.columns = ['DATE', 'PRICE']
+                price_data['DATE'] = [datetime.datetime.strptime(x, "%Y-%m-%d").date() for x in price_data['DATE']]
 
                 # other useful data on the last date: sec_id, sec_nm, any analytics, etc.
                 other_data = data.iloc[-1].to_dict()
@@ -85,6 +107,8 @@ class CovarEstimator:
                 if not price_data['PRICE'].isna().values.any():
                     sec_id_to_price[sec_id] = price_data
                     continue
+
+                print(f"""WARNING: price data in {file} have NA; use previous valid price to impute ...""")
 
                 # if there is NA in 'PRICE', impute using previous 'PRICE'
                 print(sec_id + ' has NONE in CLOSE')
@@ -101,26 +125,95 @@ class CovarEstimator:
 
         return sec_id_to_last_data, sec_id_to_price
 
-    def __init__(self, data_dir: str, rtrn_method: str, halflife_in_yrs: float, cash_rtrn: float) -> None:
+    def __init__(self, data_dir: str,
+                 rtrn_method: str,
+                 halflife_in_yrs: float,
+                 cash_rtrn: float,
+                 price_type: str,
+                 sec_id_list: List[str] = None,
+                 start_dt: datetime.date = None,
+                 end_dt: datetime.date = None) -> None:
         """
         Args:
             data_dir (str): a directory which holds all the security price data.
             rtrn_method (str): 'simple' or 'logarithm' for return calculation.
             halflife_in_yrs (float): halflife for the decay factor in covariance weighting scheme.
+            cash_rtrn (float): risk free cash rate.
+            price_type (str): type of price used for return calculation, e.g. Close, Adjust Close, etc.
+            sec_id_list (List[str]): list of securities used to covar estimation.
+            start_dt (datetime.date): start date of estimation window.
+            end_dt (datetime.date); end date of estimation window.
         """
+        if data_dir is None:
+            raise ValueError('Input data directory is None')
+        if not isinstance(data_dir, str):
+            raise ValueError(f"""Input data directory is not of type str: {data_dir}""")
+        if not os.path.exists(data_dir):
+            raise ValueError(f"""Input data directory does not exist: {data_dir}""")
+
+        if rtrn_method is None or not isinstance(rtrn_method, str) or rtrn_method not in ('simple', 'logarithm'):
+            raise ValueError("Input method is not valid")
+
+        if halflife_in_yrs is None or not isinstance(halflife_in_yrs, float):
+            raise ValueError("Input halflife_in_yrs is not valid")
+
+        if cash_rtrn is None or not isinstance(cash_rtrn, float):
+            raise ValueError("Input cash_rtrn is not valid")
+
+        if price_type is None:
+            raise ValueError('Input price type is None')
+        if not isinstance(price_type, str):
+            raise ValueError(f"""Input price type is not of type str: {price_type}""")
+
+        if sec_id_list is not None:
+            if not isinstance(sec_id_list, list):
+                raise ValueError(f"""Input sec_id_list is not of type list""")
+            if len(sec_id_list) == 0:
+                raise ValueError("Input sec_id_list is empty")
+
+        if start_dt is not None and end_dt is not None:
+            if not isinstance(start_dt, datetime.date):
+                raise ValueError(f"""Input start_dt is not of type datetime.date: {start_dt}""")
+            if not isinstance(end_dt, datetime.date):
+                raise ValueError(f"""Input end_dt is not of type datetime.date: {end_dt}""")
+            if start_dt >= end_dt:
+                raise ValueError(f"""start_dt {start_dt} is not before end_dt {end_dt}""")
+
         self._sec_id_list = None
         self._sec_id_to_last_data = None
         self._avg_rtrns = dict()
         self._covar_matrix = None
         self._risks = dict()
 
-        self._sec_id_to_last_data, sec_id_to_price = CovarEstimator._preprocess_data(data_dir)
+        self._sec_id_to_last_data, sec_id_to_price = CovarEstimator._preprocess_data(data_dir=data_dir, price_type=price_type)
+
+        # remove sec_id that are not needed
+        if sec_id_list is not None:
+            # check all sec_id's in input list have data
+            for x in sec_id_list:
+                if x not in sec_id_to_price:
+                    raise ValueError(f"""data for sec_id {x} are not in {data_dir}""")
+            # remove sec_id's that are not needed
+            for x in list(sec_id_to_price.keys()):
+                if x not in sec_id_list:
+                    self._sec_id_to_last_data.pop(x)
+                    sec_id_to_price.pop(x)
+
+        # remove data that are outside [start_dt, end_dt]
+        if start_dt is not None and end_dt is not None:
+            for sec_id, data in sec_id_to_price.items():
+                if data['DATE'].iloc[0] > start_dt or data['DATE'].iloc[-1] < end_dt:
+                    raise ValueError(f"""price data for {sec_id} cover [{data['DATE'].iloc[0]}, {data['DATE'].iloc[-1]}], but not the asked time window [{start_dt}, {end_dt}]""")
+                data = data[(data['DATE'] >= start_dt) & (data['DATE'] <= end_dt)]
+
         self._sec_id_list = sorted([x for x in self._sec_id_to_last_data])
+
+        print(f"""Estimating covariance for {self._sec_id_list} by {rtrn_method} returns: {start_dt} - {end_dt}""")
 
         rtrns_dict = dict()
         for sec_id in sec_id_to_price:
             # calculate and store return time series for each security
-            rtrn = CovarEstimator._calc_rtrns(price=sec_id_to_price.get(sec_id)['PRICE'],
+            rtrn = CovarEstimator._calc_rtrns(price=sec_id_to_price.get(sec_id)['PRICE'].to_list(),
                                               method=rtrn_method)
             dates = sec_id_to_price.get(sec_id)['DATE'][1:]
             rtrns_dict[sec_id] = pd.DataFrame.from_dict({'DATE': dates, 'RETURN': rtrn}).set_index(keys=['DATE'],
